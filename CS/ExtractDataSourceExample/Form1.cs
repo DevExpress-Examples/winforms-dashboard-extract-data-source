66fecf6bbb8c548469dd818ed4d03b7a147a2f67
Natalia Kazakova <17986517+natakazakova@users.noreply.github.com>
2021-07-01 19:10:34 +0300

    Update Form1.cs

--- CS/ExtractDataSourceExample/Form1.cs
+++ CS/ExtractDataSourceExample/Form1.cs
@@ -3,31 +3,26 @@ using DevExpress.DashboardWin;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace ExtractDataSourceExample
-{
-    public partial class Form1 : DevExpress.XtraEditors.XtraForm
-    {
-        public Form1()
-        {
+namespace ExtractDataSourceExample {
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm {
+        public Form1() {
             InitializeComponent();
             dashboardViewer1.CustomizeDashboardTitle += DashboardViewer1_CustomizeDashboardTitle;
             dashboardViewer1.LoadDashboard("DashboardTest.xml");
         }
 
         delegate void SafeUpdate(string file);
-        private void CreateExtractAndSave()
-        {
+        private void CreateExtractAndSave() {
             DataSourceCollection dsCollection = new DataSourceCollection();
             dsCollection.AddRange(dashboardViewer1.Dashboard.DataSources.Where(d => !(d is DashboardExtractDataSource)));
-            foreach (var ds in dsCollection)
-            {
+            foreach (var ds in dsCollection) {
                     DashboardExtractDataSource extractDataSource = new DashboardExtractDataSource();
                     extractDataSource.ExtractSourceOptions.DataSource = ds;
 
                     if (ds is DashboardSqlDataSource)
                         extractDataSource.ExtractSourceOptions.DataMember = ((DashboardSqlDataSource)(ds)).Queries[0].Name;
 
-                extractDataSource.FileName = "Extract_" + ds.Name + ".dat";
+                    extractDataSource.FileName = "Extract_" + ds.Name + ".dat";
                     extractDataSource.UpdateExtractFile();
                     foreach (DataDashboardItem item in dashboardViewer1.Dashboard.Items)
                         if (item.DataSource == ds)
@@ -37,49 +32,39 @@ namespace ExtractDataSourceExample
             dashboardViewer1.Dashboard.SaveToXml("Dashboard_Extract.xml");
         }
 
-        private void UpdateExtract()
-        {
+        private void UpdateExtract() {
             dashboardViewer1.ReloadData();
-            foreach (var ds in dashboardViewer1.Dashboard.DataSources.Where(d => d is DashboardExtractDataSource))
-            {
+            foreach (var ds in dashboardViewer1.Dashboard.DataSources.Where(d => d is DashboardExtractDataSource)) {
                 ((DashboardExtractDataSource)ds).UpdateExtractFile();
             }
         }
 
-        private void UpdateExtractAsync()
-        {
-            dashboardViewer1.UpdateExtractDataSourcesAsync((a, b) => { OnDataReady(a); }, (a, __) => { MessageBox.Show($"File {a} updated "); });
+        private void UpdateExtractAsync() {
+            dashboardViewer1.UpdateExtractDataSourcesAsync((a, b) => { OnDataReady(a); }, (a, __) => { 
+                MessageBox.Show($"File {a} updated "); 
+            });
         }
 
-        void OnDataReady(string fileName)
-        {
+        void OnDataReady(string fileName) {
             dashboardViewer1.Invoke(new SafeUpdate(UpdateViewer), new object[] { fileName });
         }
-        void UpdateViewer(string fileName)
-        {
+        void UpdateViewer(string fileName) {
             MessageBox.Show($"Data for the file {fileName} is ready ");
             dashboardViewer1.ReloadData();
         }
 
-        private void DashboardViewer1_CustomizeDashboardTitle(object sender, CustomizeDashboardTitleEventArgs e)
-        {
-            DashboardToolbarItem itemUpdate = new DashboardToolbarItem(
-                (args) => UpdateExtract())
-            {
-                Caption = "Update Extract Data Source",
-            };
+        private void DashboardViewer1_CustomizeDashboardTitle(object sender, CustomizeDashboardTitleEventArgs e) {
+            DashboardToolbarItem itemUpdate = new DashboardToolbarItem((args) => UpdateExtract()) {
+				Caption = "Update Extract Data Source",
+			};
             e.Items.Add(itemUpdate);
 
-            DashboardToolbarItem itemUpdateAsync = new DashboardToolbarItem(
-                (args) => UpdateExtractAsync())
-            {
-                Caption = "Async Update Extract Data Sources",
-            };
+            DashboardToolbarItem itemUpdateAsync = new DashboardToolbarItem((args) => UpdateExtractAsync()) {
+				Caption = "Async Update Extract Data Sources",
+			};
             e.Items.Add(itemUpdateAsync);
 
-            DashboardToolbarItem itemSave = new DashboardToolbarItem(
-                (args) => CreateExtractAndSave())
-            {
+            DashboardToolbarItem itemSave = new DashboardToolbarItem((args) => CreateExtractAndSave()) {
                 Caption = "Create Extract Data Source",
             };
             e.Items.Insert(0,itemSave);
